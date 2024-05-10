d7f58a5fd89ba14e36451e874189bad53f5fdbda
DrSlamp <six.stream@gmail.com>
2024-05-10 10:24:05 -0400

    AB test

---
+++ AB.py
@@ -0,0 +1,50 @@
+import numpy as np
+import math
+from numpy import random
+
+#ROWS = 4
+#COLS = 4
+
+til = 0 
+til1 = 0
+
+r = np.random.random()
+
+print(r)
+
+ROWS = random.randint(20)
+COLS = random.randint(20)
+
+print(ROWS)
+print(COLS)
+
+A= "probando"
+B= " phyton es muy chevere"
+A+B
+print (A+B)
+
+f = np.random.randint(100, size=(1))
+print(f)
+
+n = np.array([2,2,2])
+
+m = np.array([1,5,2])*n
+print (m)
+
+x = np.array(((1,2),(3, 4)))
+y = np.array(((5,6),(7, 8)))
+x + y
+
+x1 = np.matrix(((1,2),(3, 4)))
+y2 = np.matrix(((5,6),(7, 8)))
+x1 * y2
+y2 * x1
+
+print("flag")
+print(np.random.randint(16,32))
+
+chiste = pow(10000,0.5)
+
+print(chiste)
+
+
